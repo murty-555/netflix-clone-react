ba8ef3a13b4680a1e7365859e83efc6254af24b8
Murthy Kolati <krlvmurthy96@gmail.com>
2025-08-08 18:19:01 +0530

    responsiveness, spinner, toastify

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,12 +5,11 @@ import search_icon from "../../assets/search_icon.svg";
 import bell_icon from "../../assets/bell_icon.svg";
 import profile_img from "../../assets/profile_img.png";
 import caret_icon from "../../assets/caret_icon.svg";
-import { useNavigate } from "react-router-dom";
+import { logout } from "../../firebase";
 
 const Navbar = () => {
 
   const navRef = useRef();
-  const navigate = useNavigate()
 
   useEffect(() => {
     window.addEventListener("scroll", () => {
@@ -43,7 +42,7 @@ const Navbar = () => {
           <img src={profile_img} alt="" className="profile" />
           <img src={caret_icon} alt="" />
           <div className="dropdown">
-            <p onClick={() => navigate("/login")}>Sign out of netflix</p>
+            <p onClick={() => logout()}>Sign out of netflix</p>
           </div>
         </div>
       </div>
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import "./Login.css";
 import logo from "../../assets/logo.png";
+import netflix_spinner from '../../assets/netflix_spinner.gif'
 import { login, signup } from "../../firebase";
 
 const Login = () => {
@@ -8,17 +9,23 @@ const Login = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
 
   const user_auth = async (e) => {
     e.preventDefault()
+    setLoading(true)
     if(signState === "Sign In"){
       await login(email, password)
     }else{
       await signup(name, email, password)
     }
+    setLoading(false)
   } 
 
   return (
+    loading ? <div className="login-spinner">
+      <img src={netflix_spinner} alt="" />
+    </div> : 
     <div className="login">
       <img src={logo} alt="" className="login-logo" />
       <div className="login-form">
